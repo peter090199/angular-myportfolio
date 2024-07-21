9b371bd4af6efbfe1e728477a19df1ccf0dc7dc9
pedroyorpo <pedroyorpo17@gmail.com>
2024-07-22 00:46:28 +0800

    update

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './app/home/home.component';
 import { AboutComponent } from './app/about/about.component';
+import { EducationComponent } from './app/education/education.component';
+import { SkillsComponent } from './app/skills/skills.component';
+import { WorkComponent } from './app/work/work.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   // other routes
   { path: '', redirectTo: '/home', pathMatch: 'full' },// Redirect to home by default
   { path: 'about', component: AboutComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'work', component: WorkComponent },
  
 ];
 
