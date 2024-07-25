b1cbc3f9dc98119d3dbdebee226cdce7b31db196
pedroyorpo <pedroyorpo17@gmail.com>
2024-07-25 22:41:22 +0800

    commit

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { slideUpDownAnimation } from '../animation';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
+  animations:[slideUpDownAnimation]
 })
 export class SkillsComponent implements OnInit {
 
